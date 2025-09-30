6627200437930c2d6bd0073169f35962dc37bf3e
Khuongnd23 <Khuongnd23@gmail.com>
2025-10-01 00:08:59 +0700

    Create: model about Companies, Users

---
+++ src/VCareer.Domain/Models/Companies/CompanyIndustry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace VCareer.Models.Companies
+{
+    public class CompanyIndustry:Entity<int>
+    {
+        public int CompanyId { get; set; }
+        public int IndustryId { get; set; }
+        public Company Company { get;private  set; }
+        public Industry Industry { get; private set; }
+    }
+}
---
+++ src/VCareer.Domain/Models/Users/RecruiterProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VCareer.Models.Companies;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Identity;
+
+namespace VCareer.Models.Users
+{
+    public class RecruiterProfile : FullAuditedAggregateRoot<Guid>
+    {
+        public Guid UserId { get; set; }
+        public int CompanyId { get; set; }
+        public bool IsLead { get; set; }
+        public bool Status { get; set; }
+        public IdentityUser User { get; set; }
+        public Company Company { get; set; }
+    }
+}
