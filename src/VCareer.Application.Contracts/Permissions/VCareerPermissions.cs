f3b6cb03bff5338bdfbee1508765ccdf44da1f40
quangVVHE172538 <quangvvhe172538@fpt.edu.vn>
2025-10-10 11:27:31 +0700

    update DB and CVmanagement

--- src/VCareer.Application.Contracts/Permissions/VCareerPermissions.cs
+++ src/VCareer.Application.Contracts/Permissions/VCareerPermissions.cs
@@ -26,6 +26,18 @@ public static class VCareerPermissions
         public const string DeleteSupportingDocument = Default + ".DeleteSupportingDocument";
         public const string DownloadSupportingDocument = Default + ".DownloadSupportingDocument";
     }
+
+    public static class CV
+    {
+        public const string Default = GroupName + ".CV";
+        public const string CreateOnline = Default + ".CreateOnline";
+        public const string Upload = Default + ".Upload";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+        public const string Get = Default + ".Get";
+        public const string SetDefault = Default + ".SetDefault";
+        public const string SetPublic = Default + ".SetPublic";
+    }
     
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
