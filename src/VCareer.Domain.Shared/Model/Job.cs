f7c3d9343be078087e24cade7517636f7518e71a
phamducong2kx <malzaharvn1230@gmail.com>
2025-10-11 10:57:57 +0700

    them cac enum cho job vao  data.share

---
+++ src/VCareer.Domain.Shared/Model/Job.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCareer.Model
+{
+
+    // enum laoi hinh cong viec
+    public enum EmploymentTye
+    {
+        FullTime,
+        PartTime,
+        Contract,
+        Intership
+
+    }
+    //enum cho job status
+    public enum JobStatus
+    {
+        Draft,
+        Open,
+        Closed
+    }
+    public class Job
+    {
+        
+    }
+}
