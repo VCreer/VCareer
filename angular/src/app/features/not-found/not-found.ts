8b1e999a58b72c00d5e9c00ce078bf6cdaf228af
phamducong2kx <malzaharvn1230@gmail.com>
2025-11-07 08:33:17 +0700

    done list danh sach cong ty

---
+++ angular/src/app/features/not-found/not-found.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './not-found.html',
+  styleUrls: ['./not-found.scss']
+})
+export class NotFoundComponent {
+  constructor(private router: Router) {}
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
+
+  goBack() {
+    window.history.back();
+  }
+}
+
