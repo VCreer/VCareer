6743462eced7a18bccef20bd89cefc86a393cbfa
phamducong2kx <malzaharvn1230@gmail.com>
2025-12-08 22:37:35 +0700

    update code quang

--- src/VCareer.EntityFrameworkCore/Migrations/20251208090323_updateDatabse1.Designer.cs
+++ src/VCareer.EntityFrameworkCore/Migrations/20251208090323_updateDatabse1.Designer.cs
@@ -13,8 +13,8 @@ using Volo.Abp.EntityFrameworkCore;
 namespace VCareer.Migrations
 {
     [DbContext(typeof(VCareerDbContext))]
-    [Migration("20251206170346_InitDb")]
-    partial class InitDb
+    [Migration("20251208090323_updateDatabse1")]
+    partial class updateDatabse1
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- src/VCareer.EntityFrameworkCore/Migrations/20251208090323_updateDatabse1.cs
+++ src/VCareer.EntityFrameworkCore/Migrations/20251208090323_updateDatabse1.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace VCareer.Migrations
 {
     /// <inheritdoc />
-    public partial class InitDb : Migration
+    public partial class updateDatabse1 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
